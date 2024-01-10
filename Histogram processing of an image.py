d86055bb9023a00d9af28b1d39ea32775902c405
SWEDHA V <swedhavelraj@gmail.com>
2024-01-10 12:50:25 +0530

    check

--- Histogram processing of an image.py
+++ Histogram processing of an image.py
@@ -21,17 +21,14 @@ import matplotlib.pyplot as plt
 grayscale_image=cv2.imread("gray image of flower.jpg")
 colourscale_image=cv2.imread("color image of flower.jpg")
 hist=cv2.calcHist(grayscale_image,[0],None,[255],[0,255])
-hist1=cv2.calcHist(colourscale_image,[1],None,[255],[0,255])
+hist1=cv2.calcHist()
 plt.figure()
 plt.title("Histogram")
 plt.xlabel("grayscale")
 plt.ylabel("pixel count")
-plt.stem(hist)
+plt.stem())
 plt.show()
-plt.xlabel("colour scale")
-plt.ylabel("pixel count")
-plt.stem(hist1)
-plt.show()
+
 
 
 # In[3]:
